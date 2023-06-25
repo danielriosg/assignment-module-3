74bda4075187f99a1cdc0081cd9358d27073bf18
Daniel Rios <ever.rios.gonzalez@gmail.com>
2023-06-25 14:15:53 -0600

    Complete writePassword function by adding character confirmation variables, alerts password generation and missing character types, and generate the var password with the criteria

--- script.js
+++ script.js
@@ -9,10 +9,43 @@ function writePassword() {
   var passwordLength = parseInt(
     prompt("Enter the length of the password, between 8 and 128 characters")
   );
-}
 
-// Validate password length
-if (isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128) {
-  alert("Invalid password length. Please enter a number between 8 and 128");
-  return;
+  // Validate password length
+  if (isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128) {
+    alert("Invalid password length. Please enter a number between 8 and 128");
+    return;
+  }
+
+  // Confirm characters
+  var includeLowercase = confirm(
+    "Do you want to include lowercase characters?"
+  );
+  var includeUppercase = confirm(
+    "Do you want to inlcude uppercase characters?"
+  );
+  var includeNumeric = confirm("Do you want to include numbers?");
+  var includeSpecial = confirm("Do you want to include special characters?");
+
+  // Validate at least one type character is selected
+  if (
+    !includeLowercase &&
+    !includeUppercase &&
+    !includeNumeric &&
+    includeSpecial
+  ) {
+    alert("You must select at least one character type");
+    return;
+  }
+
+  // Generate the password criteria
+  var password = generatePassword(
+    passwordLength,
+    includeLowercase,
+    includeUppercase,
+    includeNumeric,
+    includeSpecial
+  );
+
+  // alert for generated password
+  alert("Your generated password is: " + password);
 }
