b24c29a1c55e292e9f9c2995094e4a97f1896d53
Daniel Rios <ever.rios.gonzalez@gmail.com>
2023-06-25 12:40:37 -0600

    create password promt and length validation

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
-
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// Create function to prompt the password with the required criteria
+function writePassword() {
+  var passwordLength = parseInt(
+    prompt("Enter the length of the password, between 8 and 128 characters")
+  );
+}
+
+// Validate password length
+if (isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128) {
+  alert("Invalid password length. Please enter a number between 8 and 128");
+  return;
+}
